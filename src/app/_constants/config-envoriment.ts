a2e37e04ba0c78201be70319efa9603688589cbc
Minh Hải Nguyễn <Minh Hi Nguyn>
2017-07-08 00:32:59 +0700

    Change

--- src/app/_constants/config-envoriment.ts
+++ src/app/_constants/config-envoriment.ts
@@ -1,4 +1,4 @@
 'use strict';
 
-export const BACKEND_SERVICE_HOST = '//localhost';
-export const BACKEND_SERVICE_PORT = 8081;
+export const BACKEND_SERVICE_HOST = 'https://backend-social.herokuapp.com';
+export const BACKEND_SERVICE_PORT = 80;
